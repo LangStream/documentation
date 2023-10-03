6287fe4f2d0f51181076f0a13f131ee41f47a3b4
Nicolò Boschi <boschi1997@gmail.com>
2023-10-03 10:55:50 +0200

    Add API reference for resources (#90)

--- dev-scripts/main.py
+++ dev-scripts/main.py
@@ -18,12 +18,15 @@ def escape_markdown(text, link = None):
     
 
 
-def generate_agent_table(agent_name, properties, is_nested=False):
+def generate_single_entity_table(entity_name, entity_ref, description, properties, is_nested=False):
     result = []
     if is_nested:
-        table = f"\n\n#### <a name=\"{agent_name}\"></a>{agent_name}\n\n"
+        table = f"\n\n#### <a name=\"{entity_ref}\"></a>{entity_name}\n\n"
     else:
-        table = f"\n\n### <a name=\"{agent_name}\"></a>{agent_name}\n\n"
+        table = f"\n\n### <a name=\"{entity_ref}\"></a>{entity_name}\n\n"
+
+    if description:
+        table += f"{escape_markdown(description)}\n\n"
 
     table += "| Key | Description | Type | Required | Default Value |\n"
     table += "| --- | --- | --- | --- | --- |\n"
@@ -36,11 +39,11 @@ def generate_agent_table(agent_name, properties, is_nested=False):
                 items = value.get('items', {})
                 if items:
                     if items.get('type', '') == "object":
-                        link = f"#{agent_name}.{key}"
+                        link = f"#{entity_ref}.{key}"
                         prop_type = f"array of object"
                     else:
                         prop_type = f"array of {items.get('type', '')}"
-            table += f"| {escape_markdown(key)} | {escape_markdown(value.get('description', ''))} | {escape_markdown(prop_type, link)} | {escape_markdown(value.get('required', ''))} | {escape_markdown(value.get('defaultValue', ''))} |\n"
+            table += f"| `{escape_markdown(key)}` | {escape_markdown(value.get('description', ''))} | {escape_markdown(prop_type, link)} | {escape_markdown(value.get('required', ''))} | {escape_markdown(value.get('defaultValue', ''))} |\n"
 
     result.append(table)
 
@@ -48,37 +51,28 @@ def generate_agent_table(agent_name, properties, is_nested=False):
         for key, value in properties.items():
             nested_properties = value.get('properties', {})
             if nested_properties:
-                nested_tables = generate_agent_table(f"{agent_name}.{key}", nested_properties, is_nested)
+                nested_tables = generate_single_entity_table(f"{entity_name}.{key}", f"{entity_ref}.{key}", '', nested_properties, is_nested)
                 result.extend(nested_tables)
             items = value.get('items', {})
             if items and items.get('properties', {}):
-                result.extend(generate_agent_table(f"{agent_name}.{key}", items.get('properties', {}), True))
+                result.extend(generate_single_entity_table(f"{entity_name}.{key}", f"{entity_ref}.{key}", '', items.get('properties', {}), True))
 
     return result
 
-def generate_agent_tables(input_file, output_file):
+def generate_entity_tables(input_file, output_file):
     # Read the JSON file
     with open(input_file, 'r') as file:
         data = json.load(file)
 
     agents_data = data.get('agents', {})
+    resources_data = data.get('resources', {})
 
     markdown_content = "# API Reference\n\n"
-    markdown_content += "## Agents\n\n"
-    markdown_content += "| ID | Name | Description |\n"
-    markdown_content += "| --- | --- | --- |\n"
-
-    for agent_name, agent_data in agents_data.items():
-        link = f"#{agent_name}"
-        markdown_content += f"| {escape_markdown(agent_name, link)} | {escape_markdown(agent_data.get('name', ''))} | {escape_markdown(agent_data.get('description', ''))} |\n"
-
-    for agent_name, agent_data in agents_data.items():
-        if agent_data:
-            tables = generate_agent_table(agent_name, agent_data.get("properties", {}))
-            for nested_table in tables:
-                markdown_content += nested_table
-
-
+    markdown_content += "- [Resources](#resources)\n"
+    markdown_content += "- [Agents](#agents)\n"
+    markdown_content += "\n\n"
+    markdown_content += gen_entity("Resources", resources_data)
+    markdown_content += gen_entity("Agents", agents_data)
             
 
     # Save the Markdown content to a file
@@ -87,6 +81,26 @@ def generate_agent_tables(input_file, output_file):
 
     print(f"Markdown tables generated and saved to {output_file}")
 
+def gen_entity(title, data):
+    markdown_content = f"## {title}\n\n"
+    markdown_content += "| ID | Name | Description |\n"
+    markdown_content += "| --- | --- | --- |\n"
+
+    for key, value in data.items():
+        label = value.get('type', key)
+           
+        link = f"#{key}"
+        markdown_content += f"| {escape_markdown(label, link)} | {escape_markdown(value.get('name', ''))} | {escape_markdown(value.get('description', ''))} |\n"
+
+    for key, value in data.items():
+        if value:
+            label = f"{value.get('name')} (`{value.get('type', key)}`)"
+            tables = generate_single_entity_table(label, key, value.get('description', ''), value.get("properties", {}))
+            for nested_table in tables:
+                markdown_content += nested_table
+    markdown_content += "\n\n"
+    return markdown_content
+
 def main():
     parser = argparse.ArgumentParser(description='Generate Markdown tables from JSON data.')
     parser.add_argument('input_file', type=str, help='Path to the input JSON file')
@@ -94,7 +108,7 @@ def main():
 
     args = parser.parse_args()
 
-    generate_agent_tables(args.input_file, args.output_file)
+    generate_entity_tables(args.input_file, args.output_file)
 
 if __name__ == '__main__':
     main()
